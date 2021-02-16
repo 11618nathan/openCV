1a1f26780cf68bd4db07a35058433d3598fe90af
Baek Gi Chan <11618nathan@gmail.com>
2021-02-17 00:08:47 +0900

    print openCV version

---
+++ openCVverion.cpp
@@ -0,0 +1,16 @@
+
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	// OpenCV 라이브러리의 버전 정보를 문자열 형태로 정의한 매크로
+	cout << "openCV VERSION: " << CV_VERSION << endl;
+
+	cout << "codeomni.tistory.com" << endl;
+
+	return 0;
+}
